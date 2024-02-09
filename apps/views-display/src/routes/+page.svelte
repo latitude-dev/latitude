fb894686427c28a8305fed801771578f746fa42f
andresgutgon <andresgutgon@gmail.com>
2024-02-09 18:10:56 +0100

    Hack demo effect

--- apps/views-display/src/routes/+page.svelte
+++ apps/views-display/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div style="width: 100%; height: 100%; position: relative;">
+  <iframe
+    style="width: 100%; height: 100%; position: absolute; top: 0; left: 0; border: none;"
+    src="https://latitude.so/app/share/b592cd31-1c22-40b1-bd6c-e440d98a0ac6" allowfullscreen>
+  </iframe>
+</div>
