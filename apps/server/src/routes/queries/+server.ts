165d1342ad9eaa4d517dc79d148211051ad20305
Gerard Clos <clos.gerard@gmail.com>
2024-02-12 14:15:13 +0100

    feature: backend endpoint #1

---
+++ apps/server/src/routes/queries/+server.ts
@@ -0,0 +1,34 @@
+import findQueryFile from "../../lib/findQueryFile";
+import { json } from "@sveltejs/kit";
+import { PostgresConnector } from '@latitude-dev/postgresql-connector'
+
+export async function GET({ params }: { params: { query: string } }) {
+  const { query } = params;
+  const found = await findQueryFile({ query, dirPath: "/static" });
+  if (!found) return new Response("Not found", { status: 404 });
+
+  // @ts-ignore
+  const connector = carlosFactory(found.sourcePath);
+  const coo = new PostgresConnector({
+    host: "localhost",
+    port: 543,
+    database: "test",
+    user: "test",
+    password: "test",
+  });
+
+  console.log(coo);
+
+  try {
+    const queryResult = await connector.query({
+      path: found.queryPath,
+      params,
+    });
+
+    return json(queryResult.toJSON());
+  } catch (e) {
+    // TODO: respond with an error including the details in json format
+    // @ts-ignore
+    return new Response(e.message, { status: 500 });
+  }
+}
--- packages/connectors/base/src/index.ts
+++ packages/connectors/base/src/index.ts
@@ -1,5 +1,5 @@
-import { TemplateNode } from "svelte/types/compiler/interfaces";
-import { Node, Identifier, BaseNodeWithoutComments } from "estree";
+import { Node, Identifier, BaseNodeWithoutComments } from 'estree'
+import { TemplateNode } from 'svelte/types/compiler/interfaces'
 import { parse } from 'svelte/compiler'
 
 export enum DataType {
@@ -36,13 +36,13 @@ export type SequentialCompiledParams = unknown[]
 export type KeyBasedCompiledParams = { [key: string]: unknown }
 
 export type CompiledQuery = {
-  sql: string,
+  sql: string
   params: SequentialCompiledParams | KeyBasedCompiledParams
 }
 
 export abstract class BaseConnector {
   /**
-   * Results from operations calculated while compiling the query will be added as 
+   * Results from operations calculated while compiling the query will be added as
    * additional parameters, instead of interpolations. These new parameters are called
    * phantom parameters, and they're added with a reserved prefix followed by an
    * incrementing number based on the usage. This prefix can be changed if it is not
@@ -65,7 +65,9 @@ export abstract class BaseConnector {
    * parameters that were stored while resolving the parameterised strings, and clear
    * the stored parameters for a future query.
    */
-  protected abstract popParams(): SequentialCompiledParams | KeyBasedCompiledParams
+  protected abstract popParams():
+    | SequentialCompiledParams
+    | KeyBasedCompiledParams
 
   protected abstract runQuery(request: CompiledQuery): Promise<QueryResult>
 
@@ -77,191 +79,279 @@ export abstract class BaseConnector {
   }
 
   protected compile(sql: string, params?: QueryParams): string {
-    if (params && Object.keys(params).some(key => key.startsWith(this.PHANTOM_PARAM_PREFIX))) {
-      throw new SyntaxError(`Parameter keys cannot start with ${this.PHANTOM_PARAM_PREFIX}`, { query: sql })
+    if (
+      params &&
+      Object.keys(params).some((key) =>
+        key.startsWith(this.PHANTOM_PARAM_PREFIX)
+      )
+    ) {
+      throw new SyntaxError(
+        `Parameter keys cannot start with ${this.PHANTOM_PARAM_PREFIX}`,
+        { query: sql }
+      )
     }
     const phantomParams: QueryParams = {}
-  
+
     const getVarValue = (varName: string, defaultValue?: unknown) => {
-      return params?.[varName] ?? defaultValue  
+      return params?.[varName] ?? defaultValue
     }
-  
+
     const parameteriseValue = (varName: string, defaultValue?: unknown) => {
       return this.resolve(varName, getVarValue(varName, defaultValue))
     }
-  
+
     const parseValue = (value: unknown): string => {
       // Create a new unique phantom param for this value and parameterise it, instead of directly interpolating the value
-      const phantomParamKey = `${this.PHANTOM_PARAM_PREFIX}${Object.keys(phantomParams).length}`
+      const phantomParamKey = `${this.PHANTOM_PARAM_PREFIX}${
+        Object.keys(phantomParams).length
+      }`
       phantomParams[phantomParamKey] = value
 
       return this.resolve(phantomParamKey, value)
     }
-  
-    const OPERATOR_METHODS: { [key: string]: (left: unknown, right: unknown) => unknown } = {
-      '>':  (left: unknown, right: unknown): unknown => (left as number) > (right as number),
-      '>=': (left: unknown, right: unknown): unknown => (left as number) >= (right as number),
-      '<':  (left: unknown, right: unknown): unknown => (left as number) < (right as number),
-      '<=': (left: unknown, right: unknown): unknown => (left as number) <= (right as number),
-      '==': (left: unknown, right: unknown): unknown => (left as number) === (right as number),
-      '!=': (left: unknown, right: unknown): unknown => (left as number) !== (right as number),
-      '&&': (left: unknown, right: unknown): unknown => (left as boolean) && (right as boolean),
-      '||': (left: unknown, right: unknown): unknown => (left as boolean) || (right as boolean),
-      '+':  (left: unknown, right: unknown): unknown => (left as number) + (right as number),
-      '-':  (left: unknown, right: unknown): unknown => (left as number) - (right as number),
-      '*':  (left: unknown, right: unknown): unknown => (left as number) * (right as number),
-      '/':  (left: unknown, right: unknown): unknown => (left as number) / (right as number),
-      '%':  (left: unknown, right: unknown): unknown => (left as number) % (right as number),
-      '??': (left: unknown, right: unknown): unknown => (left as unknown) ?? (right as unknown),
+
+    const OPERATOR_METHODS: {
+      [key: string]: (left: unknown, right: unknown) => unknown
+    } = {
+      '>': (left: unknown, right: unknown): unknown =>
+        (left as number) > (right as number),
+      '>=': (left: unknown, right: unknown): unknown =>
+        (left as number) >= (right as number),
+      '<': (left: unknown, right: unknown): unknown =>
+        (left as number) < (right as number),
+      '<=': (left: unknown, right: unknown): unknown =>
+        (left as number) <= (right as number),
+      '==': (left: unknown, right: unknown): unknown =>
+        (left as number) === (right as number),
+      '!=': (left: unknown, right: unknown): unknown =>
+        (left as number) !== (right as number),
+      '&&': (left: unknown, right: unknown): unknown =>
+        (left as boolean) && (right as boolean),
+      '||': (left: unknown, right: unknown): unknown =>
+        (left as boolean) || (right as boolean),
+      '+': (left: unknown, right: unknown): unknown =>
+        (left as number) + (right as number),
+      '-': (left: unknown, right: unknown): unknown =>
+        (left as number) - (right as number),
+      '*': (left: unknown, right: unknown): unknown =>
+        (left as number) * (right as number),
+      '/': (left: unknown, right: unknown): unknown =>
+        (left as number) / (right as number),
+      '%': (left: unknown, right: unknown): unknown =>
+        (left as number) % (right as number),
+      '??': (left: unknown, right: unknown): unknown =>
+        (left as unknown) ?? (right as unknown),
     }
-  
+
     function resolveNodeExpression(node: Node, localScope: Scope): unknown {
       switch (node.type) {
         case 'Literal':
-          return node.value;
-  
+          return node.value
+
         case 'Identifier':
-          if (!localScope.hasOwnProperty(node.name)) throw newSyntaxError(sql, node, `Undefined variable: ${node.name}`);
-          return localScope[node.name];
-  
+          if (!localScope.hasOwnProperty(node.name))
+            throw newSyntaxError(sql, node, `Undefined variable: ${node.name}`)
+          return localScope[node.name]
+
         case 'ObjectExpression':
           return node.properties.reduce((acc, prop) => {
-            if (prop.type !== 'Property') throw newSyntaxError(sql, node, 'Object definition can only contain properties');
-  
-            const key = prop.key as Identifier;
-            const value = resolveNodeExpression(prop.value, localScope);
-  
-            acc[key.name] = value;
-            return acc;
-          }, {} as Scope);
-  
+            if (prop.type !== 'Property')
+              throw newSyntaxError(
+                sql,
+                node,
+                'Object definition can only contain properties'
+              )
+
+            const key = prop.key as Identifier
+            const value = resolveNodeExpression(prop.value, localScope)
+
+            acc[key.name] = value
+            return acc
+          }, {} as Scope)
+
         case 'BinaryExpression':
         case 'LogicalExpression':
-          const operator = node.operator;
-  
-          if (!OPERATOR_METHODS.hasOwnProperty(operator)) throw newSyntaxError(sql, node, `Unsupported operator: ${operator}`);
-  
-          const left = resolveNodeExpression(node.left, localScope);
-          const right = resolveNodeExpression(node.right, localScope);
-  
-          return OPERATOR_METHODS[operator]?.(left, right);
-  
+          const operator = node.operator
+
+          if (!OPERATOR_METHODS.hasOwnProperty(operator))
+            throw newSyntaxError(sql, node, `Unsupported operator: ${operator}`)
+
+          const left = resolveNodeExpression(node.left, localScope)
+          const right = resolveNodeExpression(node.right, localScope)
+
+          return OPERATOR_METHODS[operator]?.(left, right)
+
         case 'CallExpression':
-          const methodName = (node.callee as Identifier).name;
-          
+          const methodName = (node.callee as Identifier).name
+
           if (methodName === 'param') {
-            const args = node.arguments.map(arg => resolveNodeExpression(arg, localScope));
-            if (!params?.hasOwnProperty(args[0] as string) && args[1] === undefined) {
-              throw newSyntaxError(sql, node, `Missing parameter: ${args[0]}`);
+            const args = node.arguments.map((arg) =>
+              resolveNodeExpression(arg, localScope)
+            )
+            if (
+              !params?.hasOwnProperty(args[0] as string) &&
+              args[1] === undefined
+            ) {
+              throw newSyntaxError(sql, node, `Missing parameter: ${args[0]}`)
             }
-            return getVarValue(args[0] as string, args[1]);
+            return getVarValue(args[0] as string, args[1])
           }
-          throw newSyntaxError(sql, node, `Unsupported function: ${methodName}`);
-  
+          throw newSyntaxError(sql, node, `Unsupported function: ${methodName}`)
+
         default:
-          throw newSyntaxError(sql, node, `Unsupported expression type: ${node.type}`);
+          throw newSyntaxError(
+            sql,
+            node,
+            `Unsupported expression type: ${node.type}`
+          )
       }
     }
-  
+
     function parseNode(node: TemplateNode, localScope: Scope = {}): string {
       if (!node) return ''
-  
-      switch (node.type) {  
+
+      switch (node.type) {
         case 'Fragment':
           return parseChildrenNodes(node.children, localScope)
-  
+
         case 'Comment':
           return ''
-  
+
         case 'Text':
           return node.raw
-          
+
         case 'MustacheTag':
           return parseNode(node.expression, localScope)
-            
+
         case 'IfBlock':
           return resolveNodeExpression(node.expression, localScope)
-          ? parseChildrenNodes(node.children, localScope)
-          : parseNode(node.else, localScope)
-  
+            ? parseChildrenNodes(node.children, localScope)
+            : parseNode(node.else, localScope)
+
         case 'ElseBlock':
           return parseChildrenNodes(node.children, localScope)
-  
+
         case 'EachBlock':
-          const iterableElement = resolveNodeExpression(node.expression, localScope)
-          if (!Array.isArray(iterableElement)) throw newSyntaxError(sql, node.expression, 'Element in #each block must be an array')
+          const iterableElement = resolveNodeExpression(
+            node.expression,
+            localScope
+          )
+          if (!Array.isArray(iterableElement))
+            throw newSyntaxError(
+              sql,
+              node.expression,
+              'Element in #each block must be an array'
+            )
           if (!iterableElement.length) return parseNode(node.else, localScope)
-  
+
           const contextVar = node.context.name
           const indexVar = node.index
-  
-          return iterableElement.map((item, index) => {
-            const childScope = {
-              ...localScope,
-              [contextVar]: item,
-              [indexVar]: index,
-            }
-            return parseChildrenNodes(node.children, childScope)
-          }).join('')
-              
+
+          return iterableElement
+            .map((item, index) => {
+              const childScope = {
+                ...localScope,
+                [contextVar]: item,
+                [indexVar]: index,
+              }
+              return parseChildrenNodes(node.children, childScope)
+            })
+            .join('')
+
         case 'Literal':
         case 'Identifier':
         case 'BinaryExpression':
         case 'LogicalExpression':
           return parseValue(resolveNodeExpression(node as Node, localScope))
-  
+
         case 'ObjectExpression':
-          throw newSyntaxError(sql, node, 'Object definitions are not allowed for interpolation')
-  
+          throw newSyntaxError(
+            sql,
+            node,
+            'Object definitions are not allowed for interpolation'
+          )
+
         case 'CallExpression':
-          const methodName = (node.callee as Identifier).name;
-          
+          const methodName = (node.callee as Identifier).name
+
           if (methodName === 'param') {
-            const args = node.arguments.map(resolveNodeExpression);
+            const args = node.arguments.map(resolveNodeExpression)
             if (!params?.hasOwnProperty(args[0]) && args[1] === undefined) {
-              throw newSyntaxError(sql, node, `Missing parameter: ${args[0]}`);
+              throw newSyntaxError(sql, node, `Missing parameter: ${args[0]}`)
             }
-            return parameteriseValue(args[0] as string, args[1]);
+            return parameteriseValue(args[0] as string, args[1])
           }
-          throw newSyntaxError(sql, node.callee, `Unsupported function: ${methodName}`);
-        
+          throw newSyntaxError(
+            sql,
+            node.callee,
+            `Unsupported function: ${methodName}`
+          )
+
         default:
           throw newSyntaxError(sql, node, `Unsupported node type: ${node.type}`)
       }
     }
-  
-    function parseChildrenNodes(children: TemplateNode[] | undefined, localScope: Scope = {}): string {
-      return children?.map((child) => {
-        if (child.type === 'ConstTag') {
-  
-          // Only allow constant tags to define variables
-          if (child.expression.type !== 'AssignmentExpression') throw newSyntaxError(sql, child.expression, 'Constant definitions must assign a value')
-          if (child.expression.operator !== '=') throw newSyntaxError(sql, child.expression, 'Constant definitions must use the = operator')
-          if (child.expression.left.type !== 'Identifier') throw newSyntaxError(sql, child.expression, 'Constant definitions must have an identifier on the left side')
-  
-          const varName = child.expression.left.name
-          const varValue = resolveNodeExpression(child.expression.right, localScope)
-  
-          localScope[varName] = varValue
-          return ''
-        }
-  
-        return parseNode(child, localScope)
-      }).join('') ?? ''
+
+    function parseChildrenNodes(
+      children: TemplateNode[] | undefined,
+      localScope: Scope = {}
+    ): string {
+      return (
+        children
+          ?.map((child) => {
+            if (child.type === 'ConstTag') {
+              // Only allow constant tags to define variables
+              if (child.expression.type !== 'AssignmentExpression')
+                throw newSyntaxError(
+                  sql,
+                  child.expression,
+                  'Constant definitions must assign a value'
+                )
+              if (child.expression.operator !== '=')
+                throw newSyntaxError(
+                  sql,
+                  child.expression,
+                  'Constant definitions must use the = operator'
+                )
+              if (child.expression.left.type !== 'Identifier')
+                throw newSyntaxError(
+                  sql,
+                  child.expression,
+                  'Constant definitions must have an identifier on the left side'
+                )
+
+              const varName = child.expression.left.name
+              const varValue = resolveNodeExpression(
+                child.expression.right,
+                localScope
+              )
+
+              localScope[varName] = varValue
+              return ''
+            }
+
+            return parseNode(child, localScope)
+          })
+          .join('') ?? ''
+      )
     }
-  
+
     const result = parse(sql)
     const compiledSql = parseNode(result.html)
       .replace(/\n/g, ' ') // Compile into a single line
       .replace(/\s+/g, ' ') // Remove extra spaces
       .trim() // Remove leading and trailing spaces
-  
+
     return compiledSql
   }
 }
 
 type Scope = { [key: string]: unknown }
-const newSyntaxError = (sql: string, node: BaseNodeWithoutComments, message: string) => {
+const newSyntaxError = (
+  sql: string,
+  node: BaseNodeWithoutComments,
+  message: string
+) => {
   return new SyntaxError(message, {
     query: sql,
     pos: {
@@ -272,8 +362,8 @@ const newSyntaxError = (sql: string, node: BaseNodeWithoutComments, message: str
       end: {
         line: node.loc?.end.line ?? 0,
         column: node.loc?.end.column ?? 0,
-      }
-    }
+      },
+    },
   })
 }
 
@@ -295,7 +385,10 @@ export class ConnectorError extends Error {}
 export class ConnectionError extends ConnectorError {}
 export class QueryError extends ConnectorError {}
 export class SyntaxError extends Error {
-  constructor(message: string, public context: QueryCompileErrorContext) {
+  constructor(
+    message: string,
+    public context: QueryCompileErrorContext
+  ) {
     super(message)
   }
 }
