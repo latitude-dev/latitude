a8b8e4e28dacb4bef1c29f7116612199fc33f1c6
Gerard Clos <clos.gerard@gmail.com>
2024-02-29 17:01:38 +0100

    feature: latitude plugin to support #await runQuery

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import autoImport from 'sveltekit-autoimport'
+import latitudePlugin from './plugins/latitude.mjs'
 
 export default defineConfig({
   plugins: [
+    latitudePlugin(),
     autoImport({
       module: {
         '@latitude-sdk/client/svelte': ['Button', 'Card'],
