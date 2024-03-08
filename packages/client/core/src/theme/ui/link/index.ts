0c00801ecf5de7d5e64666afc46176b31e9471c0
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2024-03-08 10:39:13 +0100

    Link component (#85)

---
+++ packages/client/core/src/theme/ui/link/index.ts
@@ -0,0 +1,5 @@
+import { cn } from '../../utils'
+
+export function cssClass({ className }: { className?: string | null }) {
+  return cn('inline-flex items-center justify-center gap-2 font-medium text-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-primary transition-colors disabled:cursor-not-allowed disabled:opacity-50', className)
+}
