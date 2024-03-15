84cef1eab6e28cbbd44df146e723732e0047e853
Gerard <clos.gerard@gmail.com>
2024-03-15 11:39:15 +0100

    feature: Alert component (#130)
    Implements the shadcn alert component in our svelte component lib

--- packages/client/core/src/theme/ui/index.ts
+++ packages/client/core/src/theme/ui/index.ts
@@ -1,21 +1,23 @@
+import * as alert from './alert'
 import * as button from './button'
-import * as toggle from './toggle'
-import * as toggleGroup from './toggle-group'
+import * as calendar from './calendar'
 import * as card from './card'
-import * as text from './text'
-import * as column from './column'
-import * as row from './row'
 import * as chart from './chart'
+import * as column from './column'
+import * as datePicker from './date-picker'
 import * as input from './input'
 import * as label from './label'
-import * as calendar from './calendar'
-import * as popover from './popover'
-import * as select from './select'
-import * as datePicker from './date-picker'
-import table from './table'
 import * as link from './link'
+import * as popover from './popover'
+import * as row from './row'
+import * as select from './select'
+import * as text from './text'
+import * as toggle from './toggle'
+import * as toggleGroup from './toggle-group'
+import table from './table'
 
 export {
+  alert,
   button,
   toggle,
   toggleGroup,
---
+++ packages/client/svelte/src/lib/ui/alert/alert.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements'
+  import { theme } from '@latitude-data/client'
+
+  type $$Props = HTMLAttributes<HTMLDivElement> & {
+    variant?: theme.ui.alert.Variant
+  }
+
+  let className: $$Props['class'] = undefined
+  export let variant: $$Props['variant'] = 'default'
+  export { className as class }
+</script>
+
+<div
+  class={theme.ui.alert.root.cssClass({ variant, className })}
+  {...$$restProps}
+  role="alert"
+>
+  <p>
+    <slot />
+  </p>
+</div>
