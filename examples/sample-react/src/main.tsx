00e80574af52d526e310669110ca39ca3f932e80
andresgutgon <andresgutgon@gmail.com>
2024-04-05 09:02:53 +0200

    Add react example app to the project

---
+++ examples/sample-react/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Example from './Example.tsx'
+import './index.css'
+import { LatitudeProvider } from '@latitude-data/react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <LatitudeProvider
+      apiConfig={{ host: import.meta.env.VITE_LATITUDE_HOST }}
+    >
+      <Example />
+    </LatitudeProvider>
+  </React.StrictMode>,
+)
