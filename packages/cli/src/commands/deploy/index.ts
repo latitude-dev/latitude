530eb315fdbbf3b108b83942c251575505319eea
Gerard Clos <clos.gerard@gmail.com>
2024-04-10 10:19:07 +0200

    WIP

--- packages/cli/src/commands/deploy/index.ts
+++ packages/cli/src/commands/deploy/index.ts
@@ -1,19 +1,39 @@
 import loggedIn from '$src/lib/decorators/loggedIn'
 import tracked from '$src/lib/decorators/tracked'
+import findOrCreateConfigFile from '$src/lib/latitudeConfig/findOrCreate'
 import { request } from '$src/lib/server'
 import betterSpawn from '$src/lib/spawn'
 import chalk from 'chalk'
 import { spawnSync } from 'child_process'
 
 async function loginDockerRegistry({ url, password }) {
-  // TODO
+  return new Promise<void>((resolve, reject) => {
+    betterSpawn(
+      'docker',
+      ['login', url, '--password-stdin'],
+      { stdio: 'pipe' },
+      {
+        onClose: (code) => {
+          if (code !== 0) {
+            console.error(chalk.red('Failed to login to Docker registry'))
+            reject()
+          }
+
+          resolve()
+        },
+        onStdout: (child) => {
+          child.stdin.write(`${password}\n`)
+        },
+      },
+    )
+  })
 }
 
-async function buildDockerImage() {
+async function buildDockerImage(tag) {
   return new Promise((resolve, reject) => {
     betterSpawn(
       'docker',
-      ['build', '.'],
+      ['build', '.', '-t', tag],
       { stdio: 'inherit' },
       {
         onClose: (code) => {
@@ -33,12 +53,31 @@ async function buildDockerImage() {
   })
 }
 
-async function pushDockerImage({ url, image }) {
-  // TODO
+async function pushDockerImage(tag) {
+  return new Promise<void>((resolve, reject) => {
+    betterSpawn(
+      'docker',
+      ['push', tag],
+      { stdio: 'inherit' },
+      {
+        onClose: (code) => {
+          if (code !== 0) {
+            console.error(chalk.red('Failed to push Docker image'))
+            reject()
+          }
+
+          resolve()
+        },
+      },
+    )
+  })
 }
 
-function deployCommand() {
+async function deployCommand() {
   console.log('Deploying...')
+
+  const latitudeJson = await findOrCreateConfigFile()
+  const name = latitudeJson.data.name
 
   request({ path: '/api/registry-credentials' }, async ({ err, res }) => {
     if (err) {
@@ -52,23 +91,28 @@ function deployCommand() {
       if (response.statusCode === 200) {
         try {
           const { url, password } = JSON.parse(responseBody)
+          const tag = `${url}/${name}:latest`
 
           await loginDockerRegistry({ url, password })
-          const image = await buildDockerImage()
-          console.log('image: ', image)
-
-          await pushDockerImage({ url, image })
+          await buildDockerImage(tag)
+          await pushDockerImage(tag)
 
           console.log(chalk.green('Deployed successfully!'))
         } catch (error) {
-          console.error('Failed to parse response:', (error as Error).message)
+          console.error(
+            chalk.red('Failed to deploy:', (error as Error).message),
+          )
+
+          process.exit(1)
         }
       } else {
         try {
           const response = JSON.parse(responseBody)
-          console.error(chalk.red('ERROR: ', response.error))
+          console.error(chalk.red('Failed to deploy: ', response.error))
         } catch (error) {
           console.error(responseBody)
+        } finally {
+          process.exit(1)
         }
       }
     }
