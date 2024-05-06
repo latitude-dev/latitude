7ba4dfe5bbe94145a858741faf0d71701615aafd
Andrés <andresgutgon@gmail.com>
2024-05-06 10:27:23 +0200

    Little GitHub stars banner in start/deploy commands in CLI (#367)

--- packages/cli/src/commands/constants.ts
+++ packages/cli/src/commands/constants.ts
@@ -1,3 +1,5 @@
+import colors from 'picocolors'
+
 export const APP_SERVER_FOLDER = 'apps/server'
 export const CLI_PACKAGE_NAME = '@latitude-data/cli'
 export const DEFAULT_VERSION_LIST = ['latest'] // If we fail to get the list
@@ -7,3 +9,10 @@ export const LATITUDE_GITHUB_SLUG = 'latitude-dev'
 export const PACKAGE_NAME = '@latitude-data/server'
 
 export const APP_FOLDER = `${LATITUDE_FOLDER}/app`
+export const GITHUB_STARTS_BANNER = `
+    You can help us by giving a 🌟 on GitHub
+    --------------------------------------
+    ${colors.blue('http://github.com/latitude-dev/latitude')}
+
+    Thanks! 🙏
+`
