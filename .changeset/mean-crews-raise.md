8fe3742af53d23568d9a389e1b9e8525cf252cc5
Andrés <andresgutgon@gmail.com>
2024-05-09 13:13:05 +0200

    Fix comobox button text overflow it's parent's width when space is reduce (#394)

---
+++ .changeset/mean-crews-raise.md
@@ -0,0 +1,6 @@
+---
+"@latitude-data/svelte": patch
+"@latitude-data/client": patch
+---
+
+Fix combobox button text overflowding parent's width when space is reduced
--- packages/client/svelte/src/lib/ui/button/button.stories.svelte
+++ packages/client/svelte/src/lib/ui/button/button.stories.svelte
@@ -1,6 +1,6 @@
 <script context="module" lang="ts">
+  import { CaretSort } from 'radix-icons-svelte'
   import Button from './index.svelte'
-  import Cute from '$lib/components/cute/index.svelte'
   import { Story, Template } from '@storybook/addon-svelte-csf'
   import { theme } from '@latitude-data/client'
 
@@ -16,6 +16,9 @@
         options: theme.ui.button.BUTTON_SIZES,
         control: { type: 'select' },
       },
+      ellipsis: {
+        control: { type: 'boolean' },
+      },
     },
     parameters: {
       layout: 'centered',
@@ -29,9 +32,14 @@
 </script>
 
 <Template let:args>
-  <Cute>
-    <Button {...args}>Click me</Button>
-  </Cute>
+  <div class="flex w-36 justify-start">
+    <Button {...args} class="w-full">
+      This is a long long text to show ellipsis
+      <slot slot="icon">
+        <CaretSort class={theme.ui.combobox.BUTTON_ICON_CSS_CLASS} />
+      </slot>
+    </Button>
+  </div>
 </Template>
 
 <Story name="Default" />
