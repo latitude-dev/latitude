951401d55234b729b1ad61e8797a7a3dc67d91b8
andresgutgon <andresgutgon@gmail.com>
2024-06-05 17:34:33 +0200

    feat: wip

---
+++ packages/client/svelte/src/lib/ui/columns/index.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+  export type Props = {
+    class?: string
+  }
+</script>
+
+<script lang="ts">
+  // TODO: Implement colapse bellow
+  // Use <box> component
+  import { theme } from '@latitude-data/client'
+
+  type $$Props = Props
+
+  let className: $$Props['class'] = undefined
+  export { className as class }
+
+  $: classes = theme.ui.column.cssClass({ className })
+</script>
+
+<div class={classes}>
+  <slot />
+</div>
