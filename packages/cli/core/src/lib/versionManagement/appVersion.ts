b7603793b4d865ebe9030876dafa306fa5e9193b
Carlos Sansón <carlos@latitude.so>
2024-06-10 14:02:40 +0200

    fix: prettier

--- packages/cli/core/src/lib/versionManagement/appVersion.ts
+++ packages/cli/core/src/lib/versionManagement/appVersion.ts
@@ -1,10 +1,17 @@
 import { readFileSync } from 'fs'
-import { APP_FOLDER, DEFAULT_VERSION_LIST, PACKAGE_NAME } from '../../commands/constants'
+import {
+  APP_FOLDER,
+  DEFAULT_VERSION_LIST,
+  PACKAGE_NAME,
+} from '../../commands/constants'
 import getPackageVersions from './getPackageVersions'
 
 export function getInstalledVersion(rootDir: string): string | null {
   try {
-    const packageJson = readFileSync(`${rootDir}/${APP_FOLDER}/package.json`, 'utf-8')
+    const packageJson = readFileSync(
+      `${rootDir}/${APP_FOLDER}/package.json`,
+      'utf-8',
+    )
     return JSON.parse(packageJson).version
   } catch (e) {
     // Do nothing
