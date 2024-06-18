b22b70d5818ccc76fb1e7c04c900c40875db7179
andresgutgon <andresgutgon@gmail.com>
2024-06-18 11:14:32 +0200

    Export source-manager types all in index.d.ts Before this changes a code running on a nodejs server wasn't able to see type definitions

---
+++ .changeset/hip-pears-clap.md
@@ -0,0 +1,5 @@
+---
+"@latitude-data/source-manager": minor
+---
+
+Export type definitions of source manager in index.d.ts
--- packages/source_manager/rollup.config.mjs
+++ packages/source_manager/rollup.config.mjs
@@ -1,28 +1,49 @@
 import typescript from '@rollup/plugin-typescript'
+import alias from '@rollup/plugin-alias'
+import dts from 'rollup-plugin-dts'
 import commonjs from '@rollup/plugin-commonjs'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      sourcemap: true,
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: './dist/types',
+        exclude: ['**/__tests__', '**/*.test.ts', '**/*.d.ts'],
+      }),
+      commonjs(),
+    ],
+    external: [
+      'yaml',
+      'fs',
+      'path',
+      'crypto',
+      'dotenv/config',
+      '@latitude-data/sql-compiler',
+      '@latitude-data/query_result',
+      '@dsnp/parquetjs',
+    ],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
     },
-  ],
-  plugins: [
-    typescript({
-      exclude: ['**/__tests__', '**/*.test.ts'],
-    }),
-    commonjs()
-  ],
-  external: [
-    'yaml',
-    'fs',
-    'path',
-    'crypto',
-    'dotenv/config',
-    '@latitude-data/sql-compiler',
-    '@latitude-data/query_result',
-    '@dsnp/parquetjs',
-  ],
-}
+    plugins: [
+      alias({
+        entries: [{ find: '@', replacement: './src/' }],
+      }),
+      dts(),
+    ],
+    external: (id) => id.startsWith('@'),
+  },
+]
